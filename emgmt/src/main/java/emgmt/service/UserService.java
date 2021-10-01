764ffcbb46d5993c9b4e777e4dd4c90d7e6e1292
mjaffers2020 <84789916+mjaffers2020@users.noreply.github.com>
2021-10-01 23:20:05 +0530

    Update UserService.java

--- emgmt/src/main/java/emgmt/service/UserService.java
+++ emgmt/src/main/java/emgmt/service/UserService.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
-import com.springboot.Entity.UserEntity;
-
 import emgmt.common.Constants;
 import emgmt.exception.RecordNotFoundException;
 import emgmt.model.Genders;
